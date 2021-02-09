d0bc74eb341d814318883c503183b18809a12bac
Ole Tjernstad <ott556@gmail.com>
2021-02-09 22:07:47 +0100

    quotes

---
+++ js/quotes.js
@@ -0,0 +1,66 @@
+/***
+ * `quotes` array
+
+  * Quotes object consist of: 
+  *     quote:  What's been said. (required)
+  *     source: Who said it. (required)
+  *     citation: What the source did (optional)
+  *     citation: Where it was said. (optional)
+  *     year:  When it was said. (optional)
+  *     link: Where to hear this being said. (optional)
+  */
+
+const quotes = [
+  {
+    quote:
+      "The most difficult thing is the decision to act, the rest is merely tenacity. The fears are paper tigers. You ca do anything you decide to do. You can act to change and control your life; and the procedure, the process is its own reward",
+    source: "Amelia Earhart",
+    occupation: "American aviator",
+    citation: undefined,
+    year: undefined,
+    link: "https://youtu.be/Wj7Q4_Oabuc?t=22",
+  },
+  {
+    quote:
+      "Good judgement comes from experience, and a lot of that comes from bad judgment.",
+    source: "Will Rogers",
+    occupation: "American Film Actor",
+    citation: undefined,
+    year: undefined,
+    link: "https://youtu.be/Wj7Q4_Oabuc?t=76",
+  },
+  {
+    quote: "It is far better to be alone, than to be in bad company.",
+    source: "George Washington",
+    occupation: "1st US President",
+    citation: undefined,
+    year: undefined,
+    link: undefined,
+  },
+  {
+    quote: "The unexamined life is not worth living",
+    source: "Socrates",
+    occupation: "Greek philosopher",
+    citation: "Socrates at his trial",
+    year: "399 BC",
+    link: "https://youtu.be/1mZYOhYDQog?t=21",
+  },
+  {
+    quote:
+      "I never did anything worth doing by accident, nor did any of my inventions come by accident; they came by work.",
+    source: "Plato",
+    occupation: "Athenian philosopher",
+    citation: undefined,
+    year: undefined,
+    link: "https://youtu.be/1mZYOhYDQog?t=28",
+  },
+  {
+    quote:
+      "It is during our darkest moments that we must focus to see the light.",
+    source: "Aristotle",
+    occupation: "Greek philosopher",
+    citation: undefined,
+    year: undefined,
+    link: "https://youtu.be/1mZYOhYDQog?t=42",
+  },
+];
